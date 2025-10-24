bb8049851285efb4bfea893465cfd1912c5413b7
Francisco Estevez <franciscoestevez987@gmail.com>
2025-10-23 23:35:31 -0300

    Agrega asistencia y clase

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
-import connectDB from './database/connection.js';
+import connectDB from './database/connection';
 import cursoRoutes from './routes/curso.routes.js';
 
 dotenv.config();
+
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors());
-app.use(express.json());
 connectDB();
 
+require('./models/asistencia.model');
+require('./models/clase.model');
+require('./models/curso.model');
+require('./models/establecimiento.model');
+require('./models/usuario.model');
+
+app.use(cors());
+app.use(express.json());
+
+const authRoutes = require('./routes/auth.routes');
+const establecimientosRoutes = require('./routes/establecimiento.routes');
+const asistenciasRoutes = require('./routes/asistencia.routes');
+const clasesRoutes = require('./routes/clase.routes');
+
 // Rutas
+app.use('/api/establecimientos', establecimientosRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/asistencias', asistenciasRoutes);
+app.use('/api/clases', clasesRoutes);
 app.use('/api/cursos', cursoRoutes);
 
 app.listen(port, () => {
